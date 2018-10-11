3bed50e8061ee88c2366de07e0cc7eda7d72d9e4
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-10-11 19:38:23 +0900

    App: RampUp - Moving objects in 3D space in augmented reality

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -33,6 +33,8 @@ class RampPickerVC: UIViewController {
         view.insertSubview(sceneView, at: 0)
         
         preferredContentSize = size
+        view.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.layer.borderWidth = 3.0
         
         let scene = SCNScene(named: "art.scnassets/ramps.scn")!
         sceneView.scene = scene
@@ -67,6 +69,7 @@ class RampPickerVC: UIViewController {
         if hitResults.count > 0 {
             let node = hitResults[0].node
             rampPlacerVC.onRampSelected(rampName: node.name!)
+            dismiss(animated: true, completion: nil)
            
         }
     }
